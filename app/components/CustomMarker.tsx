9918d0c321ebe30aeedd39a136d1f7bb14dc3e77
VovaEiffel <vovaeiffel@gmail.com>
2026-05-21 16:08:45 +0300

    fix leaflet mobile markers

--- app/components/CustomMarker.tsx
+++ app/components/CustomMarker.tsx
@@ -28,19 +28,19 @@ export const activeIcon = L.divIcon({
   className: "custom-marker",
 
   html: `
-    <div
-      style="
-        width: 22px;
-        height: 22px;
-        border-radius: 999px;
-        background: #ffffff;
-        border: 4px solid #ef4444;
-        box-shadow:
-          0 0 0 6px rgba(239,68,68,0.25),
-          0 0 24px rgba(239,68,68,0.6);
-      "
-    />
-  `,
+  <div
+    style="
+      width: 22px;
+      height: 22px;
+      border-radius: 999px;
+      background: #ffffff;
+      border: 4px solid #ef4444;
+      box-shadow:
+        0 0 0 6px rgba(239,68,68,0.25),
+        0 0 24px rgba(239,68,68,0.6);
+    "
+  ></div>
+`,
 
   iconSize: [22, 22],
   iconAnchor: [11, 11],
