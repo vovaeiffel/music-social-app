78fc26a2dc875aa3af1105c973ad1fc1dbf46e29
VovaEiffel <vovaeiffel@gmail.com>
2026-06-10 12:52:58 +0300

    update

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -7,7 +7,12 @@ import {
 } from "firebase/firestore";
 
 const firebaseConfig = {
-  // ... ваши конфиги
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
@@ -15,7 +20,6 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider();
 
-// Вместо простого getFirestore используем initializeFirestore с настройками
 export const db = initializeFirestore(app, {
   cacheSizeBytes: CACHE_SIZE_UNLIMITED,
 });
